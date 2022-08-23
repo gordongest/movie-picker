ec731effc86a839113750523c25a7c353f86ff27
Gordon Gest <gordon.gest@gmail.com>
2022-08-23 11:07:13 -0600

    refactor SelectedMovie to use implicit return

--- src/SelectedMovie.jsx
+++ src/SelectedMovie.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const SelectedMovie = ({ title }) => {
-    return (
-        <div className="selected-movie">
-            <h3>
-                {title}
-            </h3>
-        </div>
-    )
-}
+const SelectedMovie = ({ title }) =>
+    <div className="selected-movie">
+        <h3>
+            {title}
+        </h3>
+    </div>
 
 export default SelectedMovie;
